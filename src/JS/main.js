5e7ddeba67f795bed7f54d9e4d57fa6a88640f2a
Focah <mr.armando.sacco@gmail.com>
2024-06-29 16:02:22 +0200

    moved daypilot init to own file

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,6 +1,6 @@
 import '/src/CSS/style.css'
 import '/src/CSS/cakrro.css'
-import DayPilot from '/src/JS/daypilot/daypilot-all.min.js'
+import '/src/JS/daypilot/daypilot-init.js'
 
 import * as THREE from 'three';
 
@@ -174,42 +174,3 @@ orbitControls.addEventListener('change', function () {
 })
 
 animate();
-
-const dp = new DayPilot.Calendar("dp", {
-    locale: "it-it",
-    viewType: "Week",
-    headerDateFormat: "dddd",
-    timeRangeSelectedHandling: "Disabled",
-    eventMoveHandling: "Disabled",
-    eventResizeHandling: "Disabled",
-    eventClickHandling: "ContextMenu",
-    theme: "cakrro",
-    startDate: "2025-03-01",
-    // businessBeginsHour: 8,
-    // businessEndsHour: 20,
-    businessBeginsHour: 4,
-    businessEndsHour: 5,
-    heightSpec: "BusinessHoursNoScroll",
-    contextMenu: new DayPilot.Menu({
-        items: [
-            {
-                text: "Delete", onClick: (args) => {
-                    const dp = args.source.calendar;
-                    // dp.events.remove(args.source);
-                    console.log(dp.events);
-                    alert(dp.events);
-                }
-            }
-        ]
-    }),
-});
-dp.startDate = "2024-06-24";
-dp.events.list = [
-    {
-        id: "1",
-        start: DayPilot.Date.today().addHours(10),
-        end: DayPilot.Date.today().addHours(12),
-        text: "Event 1"
-    }
-];
-dp.init();
